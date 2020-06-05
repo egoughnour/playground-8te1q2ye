a81ebecd3d3b73ce978dd23d0c915f5e139b0846
Erik Goughnour <e.goughnour@gmail.com>
2020-06-04 21:38:07 -0500

    Update basin.py
    had to update the mapping to avoid log domain problems

--- python-project/withcolor/basin.py
+++ python-project/withcolor/basin.py
@@ -1,4 +1,4 @@
-from math import log, log2
+from math import exp
 
 MAX_ITER = 80
 
@@ -7,17 +7,20 @@ EPSILON = 0.001
 def cubicbrot(c):
     z = c
     n = 0
+    prev = EPSILON
+      
     while (abs(z) <= 2 or abs(z-1) <= 2 or abs(z+1) <= 2) and n < MAX_ITER:
         f = (z*z*z)-z
         p = 3*z*z-1
         if abs(p) <= EPSILON:
             return MAX_ITER
+        prev = z
         z = z - (f/p)
         if abs(z) <= EPSILON:
-            return n + 1 - log(log2(abs(z)))
+            return n + 1 + exp(abs(z-prev))
         if abs(z-1) <= EPSILON:
-            return n + 1 - log(log2(abs(z-1)))
+            return n + 1 + exp(abs(z-1-prev))
         if abs(z+1) <= EPSILON:
-            return n + 1 - log(log2(abs(z+1)))
+            return n + 1 + exp(abs(z+1-prev))
         n += 1
     return n
