d3a642b560643454422b0a47be6e2ea4b5773a81
Erik Goughnour <e.goughnour@gmail.com>
2020-06-04 21:24:47 -0500

    Update basin.py
    log log shouldn't be right, but we're trying it

--- python-project/withcolor/basin.py
+++ python-project/withcolor/basin.py
@@ -1,1 +1,23 @@
+from math import log, log2
 
+MAX_ITER = 80
+
+EPSILON = 0.001
+   
+def cubicbrot(c):
+    z = c
+    n = 0
+    while (abs(z) <= 2 or abs(z-1) <= 2 or abs(z+1) <= 2) and n < MAX_ITER:
+        f = (z*z*z)-z
+        p = 3*z*z-1
+        if abs(p) <= EPSILON:
+            return MAX_ITER
+        z = z - (f/p)
+        if abs(z) <= EPSILON:
+            return n + 1 - log(log2(abs(z)))
+        if abs(z-1) <= EPSILON:
+            return n + 1 - log(log2(abs(z-1)))
+        if abs(z+1) <= EPSILON:
+            return n + 1 - log(log2(abs(z+1)))
+        n += 1
+    return n
